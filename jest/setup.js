941f11bf73cf8aecb9157e041d7b18866666beb2
Eduardo Rosas <lalorosas@outlook.com>
2019-09-17 07:59:56 -0500

    Update setup.js

--- jest/setup.js
+++ jest/setup.js
@@ -1,13 +1,3 @@
-jest.mock('appcenter', () => {
-  return {
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    openURL: jest.fn(),
-    canOpenURL: jest.fn(),
-    getInitialURL: jest.fn(),
-  }
-})
-
 jest.mock('appcenter-analytics', () => {
   return {
     addEventListener: jest.fn(),
